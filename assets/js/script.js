e8902f92ce4db8c1b8d140001586c32016d9a8ae
NayanSidhu <nsidh068@uottawa.ca>
2021-06-17 20:02:26 -0400

    added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ button.addEventListener('click', function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=2fad36d0135aa7aa52c86dec12de0e72')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         var cityNameValue = data['name'];
         var tempValue = data['main']['temp'];
         var windValue = data['wind']['speed'];
@@ -20,15 +20,17 @@ button.addEventListener('click', function(){
         
         cityName.innerHTML = cityNameValue;
         temp.innerHTML = tempValue;
-        temp2.innerHTML = tempValue;
         wind.innerHTML = windValue;
         humidity.innerHTML = humidityValue; 
     
     });
 
-    
-
-
-
+  
 });
 
+button.addEventListener('click', function(){
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=metric&appid=2fad36d0135aa7aa52c86dec12de0e72')
+    .then(response => response.json())
+    .then(data => console.log(data));
+ 
+});
